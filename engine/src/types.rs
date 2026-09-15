59a9d2df8c00f98d55c96e9d7556e09595555aab
Jayson Acosta <147446000+jacosta57@users.noreply.github.com>
2026-09-14 20:43:20 -0400

    Add core types

---
+++ engine/src/types.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+enum Side {Buy, Sell}
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+enum OrderType {Limit, Market}
+
+#[derive(PartialOrd, Ord, Debug, Clone, PartialEq, Eq, Copy)] 
+struct Price(u64);
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)] 
+struct Quantity(u64);
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)] 
+struct OrderId(u64);
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)] 
+struct Sequence(u64);
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)] 
+struct Order {
+    id: OrderId,
+    side: Side,
+    order_type: OrderType,
+    price: Option<Price>,
+    quantity: Quantity,
+    sequence: Sequence,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)] 
+struct Trade {
+    maker_order_id: OrderId,
+    taker_order_id: OrderId,
+    price: Price,
+    quantity: Quantity,
+}
